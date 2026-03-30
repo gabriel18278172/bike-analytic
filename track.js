9b162816db304a24766c804832ed5baa835672ce
Gabriel187 <208294838+gabriel18278172@users.noreply.github.com>
2026-03-30 13:01:58 -0400

    Update track.js

--- track.js
+++ track.js
@@ -1,7 +1,21 @@
+let stats = {
+  visit: 0,
+  share: 0,
+  calculate: 0,
+  "tip-1": 0,
+  "tip-3": 0,
+  "tip-5": 0
+};
+
 export default async function handler(req, res) {
-  const event = req.query.event || "unknown";
+  const event = req.query.event;
 
-  console.log("EVENT:", event);
+  if (event && stats[event] !== undefined) {
+    stats[event]++;
+  }
 
-  res.status(200).json({ success: true });
+  res.status(200).json({
+    success: true,
+    stats
+  });
 }
